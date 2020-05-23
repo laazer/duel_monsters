388a3fd78f17f793976a28b7e2abc55d380c4b04
laazer <jabrandt31@gmail.com>
2020-05-23 13:00:25 -0400

    lesson 2

--- src/main/java/com/laazer/duel_monsters/DuelMonstersApplication.java
+++ src/main/java/com/laazer/duel_monsters/DuelMonstersApplication.java
@@ -1,5 +1,6 @@
 package com.laazer.duel_monsters;
 
+import com.laazer.duel_monsters.models.MonsterCard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,27 @@ public class DuelMonstersApplication {
 
 	@GetMapping("/duel")
 	public String duel()  {
-		Integer.MAX_VALUE
 		return "It's time to du du du du du du duel!!!";
+	}
+
+	@GetMapping("/blueEyesWhiteDragon")
+	public MonsterCard blueEyesWhiteDragon() {
+		MonsterCard blueEyesWhiteDragon = new MonsterCard("Blue-Eyes White Dragon", 3000, 2500,
+				"Dragon/Normal", 8, "Light");
+		return blueEyesWhiteDragon;
+	}
+
+	@GetMapping("/darkMagician")
+	public MonsterCard darkMagician() {
+		MonsterCard darkMagician = new MonsterCard("Dark Magician", 2500, 2100,
+				"Spellcaster/Normal", 7, "Dark");
+		return darkMagician;
+	}
+
+	@GetMapping("/celticGuardian")
+	public MonsterCard celticGuardian() {
+		MonsterCard celticGuardian = new MonsterCard("Celtic Guardian", 1400, 1200,
+				"Warrior", 4, "Earth");
+		return celticGuardian;
 	}
 }
